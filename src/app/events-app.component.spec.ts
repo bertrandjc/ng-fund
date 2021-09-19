b8c4e487e9ac0a22d33fdfd3411fc524686d8a30
Jean-Claude Bertrand <info@jcbsolutions.com>
2021-09-19 15:22:10 -0400

    update test (not working)

--- src/app/events-app.component.spec.ts
+++ src/app/events-app.component.spec.ts
@@ -1,29 +1,29 @@
 import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './events-app.component';
+import { EventsAppComponent } from './events-app.component';
 
-describe('AppComponent', () => {
+describe('EventsAppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        EventsAppComponent
       ],
     }).compileComponents();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(EventsAppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'ng-fund'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(EventsAppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('ng-fund');
   });
 
   it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(EventsAppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('.content span')?.textContent).toContain('ng-fund app is running!');
